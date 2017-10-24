ba3fc9af502d5c8361cedb80234dbb76e8c0a5a8
Ryan Sundberg <ryan.sundberg@gmail.com>
2017-10-24 09:05:50 -0700

    Rename to service.onesignal

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject arctype/onesignal "0.1.0-SNAPSHOT"
+(defproject co.arctype/service.onesignal "0.1.0-SNAPSHOT"
   :dependencies 
   [[org.clojure/clojure "1.8.0"]
    [arctype/service "0.1.0-SNAPSHOT"]
