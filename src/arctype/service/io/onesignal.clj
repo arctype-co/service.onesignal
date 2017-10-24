9e1005b7441f5bc2ec103e617b5602e4feace85c
Ryan Sundberg <ryan.sundberg@gmail.com>
2017-10-24 09:36:59 -0700

    Allow nil values for optional parameters

--- src/arctype/service/io/onesignal.clj
+++ src/arctype/service/io/onesignal.clj
@@ -53,57 +53,57 @@
   ; https://documentation.onesignal.com/reference
   {; The notification's content (excluding the title), a map of language codes to text for each language.
    :contents {:en S/Str} 
-   (S/optional-key :filters) [Filter]
+   (S/optional-key :filters) (S/maybe [Filter])
    ; The notification's title, a map of language codes to text for each language. 
-   (S/optional-key :headings) {:en S/Str} 
+   (S/optional-key :headings) (S/maybe {:en S/Str})
    ; The notification's subtitle, a map of language codes to text for each language
-   (S/optional-key :subtitle) {:en S/Str}
+   (S/optional-key :subtitle) (S/maybe {:en S/Str})
    ; A custom map of data that is passed back to your app.
-   (S/optional-key :data) {S/Keyword S/Any} 
+   (S/optional-key :data) (S/maybe {S/Keyword S/Any})
    ; The URL to open in the browser when a user clicks on the notification.
-   (S/optional-key :url) S/Str 
+   (S/optional-key :url) (S/maybe S/Str) 
    ; Time To Live - In seconds. The notification will be expired if the device does not come back online within this time.
-   (S/optional-key :ttl) S/Int 
+   (S/optional-key :ttl) (S/maybe S/Int) 
    ; Only one notification with the same id will be shown on the device. Use the same id to update an existing notification instead of showing a new one.
-   (S/optional-key :collapse_id) S/Str
+   (S/optional-key :collapse_id) (S/maybe S/Str)
    ; If blank the small_icon is used. Can be a drawable resource name or a URL.
    ; 256x256
-   (S/optional-key :large_icon) S/Str
+   (S/optional-key :large_icon) (S/maybe S/Str)
    ; Buttons to add to the notification. Icon only works for Android.
-   (S/optional-key :buttons) [{:id S/Str 
-                               :text S/Str 
-                               (S/optional-key :icon) S/Str}]
+   (S/optional-key :buttons) (S/maybe [{:id S/Str 
+                                        :text S/Str 
+                                        (S/optional-key :icon) S/Str}])
 
    ;; Android
 
    ; Sets the devices LED notification light if the device has one. ARGB Hex format. Example(Blue): "FF0000FF"
-   (S/optional-key :android_led_color) S/Str 
+   (S/optional-key :android_led_color) (S/maybe S/Str) 
    ; Sets the background color of the notification circle to the left of the notification text. ARGB Hex format. Example(Red): "FFFF0000"
-   (S/optional-key :android_accent_color) S/Str 
+   (S/optional-key :android_accent_color) (S/maybe S/Str) 
    ; Sets the lock screen visibility for apps targeting Android API level 21+ running on Android 5.0+ devices.
    ; 1 = Public (default) (Shows the full message on the lock screen unless the user has disabled all notifications from showing on the lock screen. Please consider the user and mark private if the contents are.)
    ; 0 = Private (Hides message contents on lock screen if the user set "Hide sensitive notification content" in the system settings)
    ; -1 = Secret (Notification does not show on the lock screen at all)
-   (S/optional-key :android_visibility) (S/enum -1 0 1) 
+   (S/optional-key :android_visibility) (S/maybe (S/enum -1 0 1))
    ; All notifications with the same group will be stacked together using Android's Notification Stacking feature.
-   (S/optional-key :android_group) S/Str
+   (S/optional-key :android_group) (S/maybe S/Str)
    ; Summary message to display when 2+ notifications are stacked together. Default is "# new messages". Include $[notif_count] in your message and it will be replaced with the current number.
    ; Example: {"en": "You have $[notif_count] new messages"}
-   (S/optional-key :android_group_message) {:en S/Str}
+   (S/optional-key :android_group_message) (S/maybe {:en S/Str})
    ; Sound file that is included in your app to play instead of the default device notification sound. NOTE: Leave off file extension for Android.
    ; Example:  "notification"
-   (S/optional-key :android_sound) S/Str
+   (S/optional-key :android_sound) (S/maybe S/Str)
 
    ;; iOS
 
    ; Describes whether to set or increase/decrease your app's iOS badge count by the ios_badgeCount specified count.
-   (S/optional-key :ios_badgeType) (S/enum "None" "SetTo" "Increase")
+   (S/optional-key :ios_badgeType) (S/maybe (S/enum "None" "SetTo" "Increase"))
    ; Used with ios_badgeType, describes the value to set or amount to increase/decrease your app's iOS badge count by.
-   (S/optional-key :ios_badgeCount) S/Int
+   (S/optional-key :ios_badgeCount) (S/maybe S/Int)
 
    ; Sound file that is included in your app to play instead of the default device notification sound. Pass  "nil" to disable vibration and sound for the notification.
    ; Example: "notification.wav"
-   (S/optional-key :ios_sound) S/Str})
+   (S/optional-key :ios_sound) (S/maybe S/Str)})
 
 (S/defn tag-filter :- Filter
   [key-name key-value]
